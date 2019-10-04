e8a570350f0c3775a0e6b48e9ba4cd47d890938d
Preston Whitehall <pmwhitehall@gmail.com>
2019-10-04 14:43:49 -0700

    Initial dependencies and framework

---
+++ challenge_3/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [
+          path.resolve(__dirname, 'client'),
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules'),
+        ],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
+}
